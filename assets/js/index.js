eb8f85d31688a1e9aac647fcc0922f51da9a166b
Shubham Pandey <103932443+Shubhamkz@users.noreply.github.com>
2023-07-25 10:21:18 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+const menuToggle = document.querySelector(".toggle_sidebar");
+const main_page = document.querySelector(".main_page");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  main_page.classList.toggle("active");
+});
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
